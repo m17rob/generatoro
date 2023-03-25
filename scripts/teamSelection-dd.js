3170607a5f933f0c17fa4ce250ca5ae27d43d657
m17rob <nar.rob17@gmail.com>
2023-03-25 12:24:57 +0200

    sal2

--- scripts/teamSelection-dd.js
+++ scripts/teamSelection-dd.js
@@ -9,6 +9,31 @@ function blockZoom() {
     lastTouchEnd = now;
   }, false);
 }
+window.addEventListener('load', function(){
+  // afișează preloader-ul
+  document.querySelector('#preloader').style.display = 'block';
+  // întârzie afișarea conținutului principal cu 2 secunde
+  setTimeout(function() {
+    // adaugă clasele animate__animated animate__zoomOut la preloader
+    document.querySelector('#preloader').classList.add('animate__animated', 'animate__fadeOut');
+    // așteaptă 1 secundă ca animația să se încheie
+    setTimeout(function() {
+      // adaugă opacitatea 0 pentru preloader
+      document.querySelector('#preloader').style.opacity = '0';
+      // așteaptă 0,5 secunde ca opacitatea să scadă la 0
+      setTimeout(function() {
+        // ascunde preloader-ul
+        document.querySelector('#preloader').style.display = 'none';
+        // adaugă clasele animate__animated animate__zoomIn la conținutul principal
+        document.querySelector('#main-content').classList.add('animate__animated', 'animate__fadeIn');
+        // afișează conținutul principal
+        document.querySelector('#main-content').style.display = 'block';
+      }, 500);
+    }, 1000);
+  }, 2000);
+});
+
+
 
 blockZoom();
 
